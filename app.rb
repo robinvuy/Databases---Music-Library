93c2bea1fc8db10f244c7061e4088895eb4360e4
Robin Vuyyuru <robinvuy@MASTUDENT-Robin-Vuyyuru-C17GMB1VQ6L4.local>
2023-03-31 09:22:43 +0100

    Phase 2- no. 5 exercise completed

--- app.rb
+++ app.rb
@@ -20,12 +20,15 @@ DatabaseConnection.connect('music_library')
 
 artist_repository = ArtistRepository.new
 
-artist_repository.all.each do |artist|
-  p artist
-end
+#artist_repository.all.each do |artist|
+  #p artist
+#end
 
 album_repository = AlbumRepository.new
 
-album_repository.all.each do |album|
-  p album
-end
+#album_repository.all.each do |album|
+  #p album
+#end
+
+album = album_repository.find(10)
+puts album.title
